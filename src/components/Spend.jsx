3961ade441de5daa7ebf01ffbe35ab08ab1283df
Carlos Londono <c.londono.m@gmail.com>
2022-06-24 18:29:58 -0500

    spend list image fixed

--- src/components/Spend.jsx
+++ src/components/Spend.jsx
@@ -1,21 +1,27 @@
 import { dateFormat } from '../helpers';
-import { images } from '../constants/images';
-// const iconsDirectory = {
+import foodIcon from '../img/food.svg';
+import healthIcon from '../img/health.svg';
+import homeIcon from '../img/home.svg';
+import leisureIcon from '../img/leisure.svg';
+import savingIcon from '../img/saving.svg';
+import subscriptionIcon from '../img/subscription.svg';
+import spendIcon from '../img/spend.svg';
 
-//             food: food
-//             health: health
-//             home: home
-//             leisure: leisure
-//             saving: saving
-//             subscriptions: subscriptions
-//             various: spends
-
-// }
+const iconsDirectory = {
+  food: foodIcon,
+  health: healthIcon,
+  home: homeIcon,
+  leisure: leisureIcon,
+  saving: savingIcon,
+  subscriptions: subscriptionIcon,
+  various: spendIcon,
+};
 const Spend = ({ spend }) => {
   const { name, amount, category, date } = spend;
   return (
     <div className='spend shade'>
       <div className='spend-content'>
+        <img src={iconsDirectory[category]} alt='spend icon' />
         <div className='spend-description'>
           <p className='category'>{category}</p>
           <p className='spend-name'>{name}</p>
