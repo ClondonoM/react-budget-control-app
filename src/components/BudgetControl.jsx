e32c88fc43882498aee86fc546c6205b28b8abfd
Carlos Londono <c.londono.m@gmail.com>
2022-06-25 17:02:32 -0500

    adding calculations and styles to progress bar

--- src/components/BudgetControl.jsx
+++ src/components/BudgetControl.jsx
@@ -3,13 +3,18 @@ import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 const BudgetControl = ({ spends, budget }) => {
+  const [percentage, setPercentage] = useState(0);
   const [available, setAvailable] = useState(0);
   const [spent, setSpent] = useState(0);
 
   useEffect(() => {
     const totalSpent = spends.reduce((total, spend) => spend.amount + total, 0);
+    const newPercentage = ((totalSpent / budget) * 100).toFixed(2);
     setSpent(totalSpent);
     setAvailable(budget - totalSpent);
+    setTimeout(() => {
+      setPercentage(newPercentage);
+    }, 1000);
   }, [spends]);
 
   const moneyFormat = (amount) => {
@@ -21,7 +26,16 @@ const BudgetControl = ({ spends, budget }) => {
   return (
     <div className='budget-container container shade two-cols'>
       <div>
-        <CircularProgressbar value={50} />
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: '#1a788a',
+            trailColor: '#f5f5f5',
+            textColor: '#1a788a',
+            textWeight: 'bold',
+          })}
+          value={percentage}
+          text={`${percentage}% Spent`}
+        />
       </div>
       <div className='budget-content'>
         <p>
